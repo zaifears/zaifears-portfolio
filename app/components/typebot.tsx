13ccc705a37f5eada97247abaf82ff02ba96d981
Zaifears Republic <alshahoriar.hossain@gmail.com>
2025-08-01 14:23:16 +0000

    Fixed the Typebot bubble position on mobile dev.

--- app/components/typebot.tsx
+++ app/components/typebot.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"; // This directive marks the component as a Client Component
 
 import { Bubble } from "@typebot.io/react";
 
@@ -6,8 +6,11 @@ export function TypebotBubble() {
   return (
     <Bubble
       typebot="cmdr248uk000bju049mwjv96l"
-      apiHost="https://typebot.io" // <-- Add this line
-      theme={{ button: { backgroundColor: "#0042DA" } }}
+      theme={{
+        button: {
+          backgroundColor: "#0042DA",
+        },
+      }}
     />
   );
 }
